c0765d549aa1637c26a42b8df8734c5af7a284d3
Nathan Haberman <na.haberman@gmail.com>
2019-03-01 16:03:03 -0600

    Got the URL to display store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <form className="store-selector">
-                <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-                <button type="submit">Visit Store</button>
-            </form >
-        )
-    }
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter a Store</h2>
+        <input
+          ref={this.myInput}
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
