a4eb10dd4a1a290fce8ba94af7d8971d79a13d80
Nathan Haberman <na.haberman@gmail.com>
2019-03-08 17:07:12 -0600

    Taking out my API keys

--- src/base.js
+++ src/base.js
@@ -2,9 +2,7 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDQ7AoIuM9ogROXMoxxYEG8RERWq6RTMA8",
-  authDomain: "catch-of-the-day-nathan-h.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-nathan-h.firebaseio.com"
+  // Taking out for security reasons
 });
 
 const base = Rebase.createClass(firebaseApp.database());
